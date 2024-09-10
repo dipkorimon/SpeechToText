7348d427b01fd9b757ba06852b6791dd8d1c61e1
dipkorimon <dipkorimon@gmail.com>
2024-09-10 12:29:50 +0600

    rename dp table to edit_distance

--- main/views.py
+++ main/views.py
@@ -24,21 +24,21 @@ def save_speech(request):
       if speech_text_length == 0 or predefined_sentence_length == 0:
         continue
 
-      dp = [[0] * (predefined_sentence_length + 1) for _ in range(speech_text_length + 1)]
+      edit_distance = [[0] * (predefined_sentence_length + 1) for _ in range(speech_text_length + 1)]
 
       for speech_index in range(speech_text_length + 1):
-        dp[speech_index][0] = speech_index
+        edit_distance[speech_index][0] = speech_index
       for predefined_index in range(predefined_sentence_length + 1):
-        dp[0][predefined_index] = predefined_index
+        edit_distance[0][predefined_index] = predefined_index
 
       for speech_index in range(1, speech_text_length + 1):
         for predefined_index in range(1, predefined_sentence_length + 1):
           if speech_text[speech_index - 1] == predefined_sentence[predefined_index - 1]:
-            dp[speech_index][predefined_index] = dp[speech_index - 1][predefined_index - 1]
+            edit_distance[speech_index][predefined_index] = edit_distance[speech_index - 1][predefined_index - 1]
           else:
-            dp[speech_index][predefined_index] = 1 + min(dp[speech_index][predefined_index - 1], dp[speech_index - 1][predefined_index], dp[speech_index - 1][predefined_index - 1])
+            edit_distance[speech_index][predefined_index] = 1 + min(edit_distance[speech_index][predefined_index - 1], edit_distance[speech_index - 1][predefined_index], edit_distance[speech_index - 1][predefined_index - 1])
 
-      minimum_operation = dp[speech_text_length][predefined_sentence_length]
+      minimum_operation = edit_distance[speech_text_length][predefined_sentence_length]
       matched_percentage = 100 - (minimum_operation / max(speech_text_length, predefined_sentence_length)) * 100
 
       if matched_percentage >= 75:
